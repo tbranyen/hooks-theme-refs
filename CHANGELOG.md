6dcb6476bac3da342b2afb0039233b5e1506f4d9
Tim Branyen <tim@tabdeveloper.com>
2017-05-09 14:38:25 -0700

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# 0.2.0
+
+- Removed lodash dependency, too heavy, in favor of a smaller replacement
+
+# 0.1.0
+
+- Allows passing a different constructor to look up `defaultProps` from
+
+# 0.0.0
+
+- Initial release
---
+++ index.js
@@ -0,0 +1,40 @@
+const defaultsDeep = (...merge) => merge.slice(1).reduce((merged, props) => {
+  props = (props && typeof props === 'object') ? props : {};
+
+  Object.keys(props).forEach(propName => {
+    if (Array.isArray(merged[propName])) {
+      merged[propName] = defaultsDeep([], merged[propName], props[propName]);
+    }
+    else if (typeof merged[propName] === 'object') {
+      merged[propName] = defaultsDeep({}, merged[propName], props[propName]);
+    }
+    else if (!(propName in merged)) {
+      merged[propName] = props[propName];
+    }
+  });
+
+  return merged;
+}, merge[0] || {});
+
+module.exports = (instance, constructor=instance.constructor) => {
+  const { hooks, theme, refs } = constructor.defaultProps;
+  const props = Object.assign({}, instance.props);
+
+  if (hooks) {
+    props.hooks = defaultsDeep(props.hooks, hooks);
+  }
+
+  if (theme) {
+    props.theme = defaultsDeep(props.theme, theme);
+  }
+
+  if (refs) {
+    props.refs = defaultsDeep(props.refs, refs);
+  }
+
+  return props;
+}
+
+// Allow for: const { htr, defaultsDeep } = require('htr');
+module.exports.htr = module.exports;
+module.exports.defaultsDeep = defaultsDeep;
